802e187d708c3cbf7d9059615f55aa255349a044
Dr. Bhagyashri A. Kelkar <kelkarba@gmail.com>
2021-10-03 21:54:17 +0530

    resources

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 # todos_controller.rb
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:due_date).map { |todo| todo.to_pleaseant_string }.join("\n")
   end
@@ -11,14 +13,15 @@ class TodosController < ApplicationController
   end
 
   def create
-    todo_text=params[:todo_text]
-    due_date=DateTime.parse(params[:due_date])
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
     new_todo = Todo.create!(
       todo_text: todo_text,
       due_date: due_date,
-      completed: false
+      completed: false,
     )
 
-    response_text="Hey, your new todo is created with the id #{new_todo.id}"
+    response_text = "Hey, your new todo is created with the id #{new_todo.id}"
     render plain: response_text
+  end
 end
